52e41750562600cd9094af47b6ae487b42b91fd0
naoma121 <naomina@me.com>
2025-11-29 05:49:02 +0900

    RSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,17 @@
 import { defineConfig } from "astro/config";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
 import tailwindcss from "@tailwindcss/vite";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
